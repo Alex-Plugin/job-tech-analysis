c360889a6b3edeb6ba42aeca777f7c7428233d1e
Alex Plugin <plugin0785@gmail.com>
2026-03-26 22:43:13 +0100

    created in config.py TECHNOLOGIES & URLs

---
+++ config.py
@@ -0,0 +1,17 @@
+TECHNOLOGIES = [
+    "Python", "Django", "Flask", "HTML", "CSS",
+    "FastAPI", "Django Rest", "REST API",
+    "C++", "JavaScript", "React", "asyncio", "Angular",
+    "CI/CD", "Odoo", "TypeScript", "SOLID Principles",
+    "Pandas", "NumPy", "Matplotlib", "TensorFlow", "PyTorch",
+    "Selenium", "Beautiful Soup", "Scrapy",
+    "SQL", "SQLAlchemy", "PostgreSQL", "SQLite", "MongoDB",
+    "Redis", "Oracle", "NoSQL", "RabbitMQ", "GraphQL",
+    "Docker", "Kubernetes", "AWS", "GCP", "Azure", "Laravel",
+    "Git", "GitHub", "Linux", "Ubuntu", "Raspberry Pi", "nginx",
+    "Spark", "Hadoop", "OOP principles", "Celery", "PyTest"
+    "LangChain", "LangGraph", "Alembic", "Agile", "Swagger",
+]
+
+SEARCH_URL = "https://www.work.ua/jobs-python/" # this is search url where we get all the vacancies
+BASE_URL = "https://www.work.ua/jobs/" # after this url goes part of url of definite vacancy - nummer.

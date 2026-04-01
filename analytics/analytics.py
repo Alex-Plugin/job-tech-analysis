eb0311315301be2478e9989e72148c194c266607
Alex Plugin <plugin0785@gmail.com>
2026-04-01 15:26:13 +0200

    Add function that plots pie graph with vacacies by city

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 from config import PLOTS_DIR
 
-# top 20 technologies bar graph
 # pie graph by cities
 # salary depending on level
 # quantity of vacancies depending on level
@@ -14,50 +13,76 @@ def load_data(path: str) -> pd.DataFrame:
     df = pd.read_csv("../data/vacancies.csv")
     return df.dropna(subset=["technologies"]) # we make dataframe without NaN
 
-# we create a function that converts str in technologies into
-# a python list with technologies
-def prepare_technologies(df: pd.DataFrame) -> pd.DataFrame:
-    def parse_technologies(tech_str: str) -> list[str]:
-        tech_str = tech_str.strip("[]")     # remove []
-        tech_str = tech_str.replace("'", "")      # remove quotes
-        # in return, we are creating a list with these technologies with help of
-        # list generator
-        return [t.strip() for t in tech_str.split(",") if t.strip()]
+# # we create a function that converts str in technologies into
+# # a python list with technologies
+# def prepare_technologies(df: pd.DataFrame) -> pd.DataFrame:
+#     def parse_technologies(tech_str: str) -> list[str]:
+#         tech_str = tech_str.strip("[]")     # remove []
+#         tech_str = tech_str.replace("'", "")      # remove quotes
+#         # in return, we are creating a list with these technologies with help of
+#         # list generator
+#         return [t.strip() for t in tech_str.split(",") if t.strip()]
+#
+#     # we convert every line of our technology dataframe
+#     df["technologies"] = df["technologies"].apply(parse_technologies)
+#     # we expand the list in every line into a column
+#     return df.explode("technologies")
+#
+# # we count how many techonlogies we have in vacancies (top 20)
+# # it creates, in a result we get, object Series with index - name of
+# #technology and value counts of these technologies
+# def plot_top_technologies(df: pd.DataFrame):
+#     tech_counts = df["technologies"].value_counts().head(20)
+#
+#     # we create a bar diagram
+#     # we can make it with this shortcut - tech_counts.plot(kind="bar")
+#     # but to make everything crear
+#
+#     fig, ax = plt.subplots()
+#
+#     ax.bar(tech_counts.index, tech_counts.values)
+#
+#     ax.set_title("Top 20 Technologies")
+#     ax.set_xlabel("Technology")
+#     ax.set_ylabel("Count")
+#
+#     ax.tick_params(axis="x", rotation=90)
+#
+#     plt.tight_layout()
+#     plt.savefig(PLOTS_DIR / "top_technologies.png")
+#     plt.show()
 
-    # we convert every line of our technology dataframe
-    df["technologies"] = df["technologies"].apply(parse_technologies)
-    # we expand the list in every line into a column
-    return df.explode("technologies")
 
-# we count how many techonlogies we have in vacancies (top 20)
-# it creates, in a result we get, object Series with index - name of
-#technology and value counts of these technologies
-def plot_top_technologies(df: pd.DataFrame):
-    tech_counts = df["technologies"].value_counts().head(20)
+def plot_vacancies_by_city(df: pd.DataFrame) -> None:
+    """
+    Builds the pie diagram and plots the vacancies by city.
+    """
+    # We count the quantity of vacancies in every city
+    city_counts = df["location"].value_counts()
 
-    # we create a bar diagram
-    # we can make it with this shortcut - tech_counts.plot(kind="bar")
-    # but to make everything crear
+    # Create a figure
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.pie(
+        city_counts.values,
+        labels=city_counts.index,
+        autopct='%1.1f%%',  # result in percents
+        startangle=90,      # we rotate the pie graph to start from the top
+        pctdistance=0.85
+    )
+    ax.set_title("Vacancies distribution by city")
 
-    fig, ax = plt.subplots()
-
-    ax.bar(tech_counts.index, tech_counts.values)
-
-    ax.set_title("Top 20 Technologies")
-    ax.set_xlabel("Technology")
-    ax.set_ylabel("Count")
-
-    ax.tick_params(axis="x", rotation=90)
-
-    plt.tight_layout()
-    plt.savefig(PLOTS_DIR / "top_technologies.png")
-    plt.show()
+    # Save the graph
+    plt.savefig(PLOTS_DIR / "vacancies_by_cities.png")
+    print(f"Saved pie chart to {PLOTS_DIR}")
 
 
 def main():
     df = load_data("../data/vacancies.csv")
-    df_exploded = prepare_technologies(df)
-    plot_top_technologies(df_exploded)
+    # df_exploded = prepare_technologies(df)
+    # plot_top_technologies(df_exploded)
+
+    df = df.dropna(subset=["location"])  # we delete all Nan from df
+    plot_vacancies_by_city(df)
 
 
 if __name__ == "__main__":
